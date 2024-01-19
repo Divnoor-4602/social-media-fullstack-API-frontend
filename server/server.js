995fc7d6d1b3f8230d8f62efe27e2947318bc671
Divnoor <divnoorsingh4602@gmail.com>
2024-01-19 19:01:28 +0530

    login auth start mongo db intergation start

--- server/server.js
+++ server/server.js
@@ -5,13 +5,20 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.post("/register", (req, res) => {
-  console.log(req.body);
+// route for registering the user in the database
+app.post("/auth/register", (req, res) => {
+  console.log("register route");
+});
+
+// route to login and verify the user
+app.post("/auth/login", (req, res) => {
+  console.log("login route");
 });
 
 app.listen(port, () => {
