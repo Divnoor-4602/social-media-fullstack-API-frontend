50730e17cb6bf6f348e828362bca752765c4b911
Divnoor <divnoorsingh4602@gmail.com>
2024-01-23 00:00:09 +0530

    readme updated

---
+++ readme.md
@@ -0,0 +1,19 @@
+# Social Media Project: Not My Space - MERN
+
+## Frameworks used
+
+- ReactJS
+- ExpressJS
+- MongoDB: Mongoose
+- NodeJS
+
+## Login / Regsiter Form
+
+The login / register form is built using **React** and **Module CSSS**, the form is repsonsive and the isnpiration of the design was taken from an online resource [themeforest](https://themeforest.net/) and the background image for the form was obtained from
+[haikei](https://app.haikei.app/).
+
+![Register form](image.png)
+
+![Login form](image-1.png)
+
+### Form Authentication and Validation
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// todo: implement a mongo database and save the user in it
 import express from "express";
 import bodyParser from "body-parser";
 import bcrypt from "bcrypt";
@@ -33,7 +32,7 @@ async function createNewUser(fullname, email, password) {
 }
 
 // route for registering the user in the database
-app.post("/auth/register", (req, res) => {
+app.post("/auth/register", async (req, res) => {
   const email = req.body.email;
   const fullname = req.body.fullname;
   const password = req.body.password;
@@ -44,26 +43,66 @@ app.post("/auth/register", (req, res) => {
   );
   // if email or password is invalid send 401 status
   if (checkPassPattern.test(password) == false) {
-    res.status(401).send("Invalid password");
+    res.status(400).send("Invalid password");
     return;
   } else {
     // password hashing
-    bcrypt.hash(password, saltRounds, (err, hashGenerated) => {
+    bcrypt.hash(password, saltRounds, async (err, hashGenerated) => {
       if (err) {
         console.log("error while hashing " + err);
       } else {
         // create a user if hashing succesful
-        // todo: Add a user only if the email is not already registered
-        createNewUser(fullname, email, hashGenerated);
-        res.status(200).send("Succesfully registerd");
+        const userExists = await User.findOne({ email: email });
+        if (userExists == null) {
+          createNewUser(fullname, email, hashGenerated);
+          res.sendStatus(200);
+          return;
+        } else {
+          res.sendStatus(404);
+          return;
+        }
       }
     });
   }
 });
 
 // route to login and verify the user
-app.post("/auth/login", (req, res) => {
+app.post("/auth/login", async (req, res) => {
   // get user details
+  const emailToVerify = req.body.email;
+  const passwordToVerify = req.body.password;
+  // fetch the user using email
+  const user = await User.findOne({ email: emailToVerify });
+  if (user != null) {
+    // compare the password to the hash in the database
+    bcrypt.compare(passwordToVerify, user.password, (error, result) => {
+      if (error) {
+        console.log(error);
+        // invalid password
+        res.sendStatus(400);
+        return;
+      } else {
+        if (result) {
+          // valid credentials login succesful
+          res.status(200).send(true);
+          return;
+        } else {
+          // invalid password
+          res.status(400).send("Invalid password");
+          return;
+        }
+      }
+    });
+  } else {
+    // invalid email
+    res.sendStatus(404);
+    return;
+  }
+});
+
+// route to update password
+app.post("/auth/forget", (req, res) => {
+  // todo: get the user by email and update their password, in the form or through sending a gmail
 });
 
 app.listen(port, () => {
